53f956fca9ac13447a9c65ca038c1a3f26575d22
Shakshi sharma <sakshisharma1495@gmail.com>
2023-09-14 17:27:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# HighLand Chocolates
+
+This is the Front-end HTML,CSS,JavaScript website. where anyone can add maximum 8 pack of Chocolates into the cart.
+
+## Functionalities-
+
+1. User can see the Chocolates.
+2. User can add Chocolates into the cart.
+3. User can check their cart and can check their added Chocolates.
+4. The added Chocolates cart can contains maximum 8 Chocolates.
+5. User can delete the added Chocolates from the cart.
+6. User can see the numbers of added Chocolates into the cart.
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,75 @@
+
+const chocolates = document.querySelectorAll('.chocolate');
+const cartItem = document.getElementById('cartItem');
+const total = document.getElementById('total');
+const checkoutButton = document.getElementById('checkout');
+let cart = [];
+let count = 0;
+
+// Looping all the add to cart chocolates and what chocolate is add to cart
+chocolates.forEach((chocolate) => {
+    const addToCartButton = chocolate.querySelector('.add-to-cart');
+    const chocolateId = chocolate.dataset.id;
+    const chocolateName = chocolate.dataset.name;
+    const chocolatePrice = chocolate.dataset.price;
+
+    addToCartButton.addEventListener('click', () => {
+        if (cart.length < 8) {
+            cart.push({ id: chocolateId, name: chocolateName, price: chocolatePrice });
+            count++;
+            document.getElementById('count').innerHTML = count;
+            updateCart();
+        } else {
+            alert('You can only select up to 8 chocolates');
+        }
+    });
+});
+
+// update cart after added the chocolates
+let totalPrice = 0;
+function updateCart() {
+    cartItem.innerHTML = '';
+    
+    cart.forEach(item => {
+        totalPrice += parseInt(item.price);
+
+        let itemHTML = `<div class="cart-item">
+            <h2>${item.name}</h2>
+            <p>Price: ${item.price}</p>
+            <i class="fa-solid fa-trash"></i>
+        </div>`;
+        
+        cartItem.innerHTML += itemHTML;
+    });
+
+    let deleteIcons = cartItem.querySelectorAll('.fa-solid.fa-trash');
+    deleteIcons.forEach((deleteIcon, index) => {
+        deleteIcon.addEventListener('click', () => {
+            removeFromCart(cart[index].id);
+        });
+    });
+
+    total.textContent = totalPrice;
+}
+
+// Remove from the cart or delete the item
+function removeFromCart(itemId) {
+    const index = cart.findIndex((item) => item.id === itemId);
+    if (index !== -1) {
+        cart.splice(index, 1);
+        count--;
+        document.getElementById('count').innerHTML = count;
+        updateCart();
+    }
+}
+
+// Click event on checkout button
+checkoutButton.addEventListener('click', () => {
+    if (cart.length === 0) {
+        alert('Your cart is empty. Please add chocolates to your cart before checking out.');
+     } 
+     else {
+        alert(`Total Price: ${totalPrice}`);
+    }
+});
+
