f6add84dd7c1359a6e877ad76649ee04445779ba
Shakshi sharma <sakshisharma1495@gmail.com>
2023-12-20 15:49:07 +0530

    after changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-
 const chocolates = document.querySelectorAll('.chocolate');
 const cartItem = document.getElementById('cartItem');
 const total = document.getElementById('total');
 const checkoutButton = document.getElementById('checkout');
 let cart = [];
 let count = 0;
+let totalPrice = 0;
 
 // Looping all the add to cart chocolates and what chocolate is add to cart
 chocolates.forEach((chocolate) => {
     const addToCartButton = chocolate.querySelector('.add-to-cart');
     const chocolateId = chocolate.dataset.id;
     const chocolateName = chocolate.dataset.name;
-    const chocolatePrice = chocolate.dataset.price;
+    const chocolatePrice = parseInt(chocolate.dataset.price);
 
     addToCartButton.addEventListener('click', () => {
         if (cart.length < 8) {
@@ -25,29 +25,32 @@ chocolates.forEach((chocolate) => {
     });
 });
 
-// update cart after added the chocolates
-let totalPrice = 0;
+// Update cart after adding the chocolates
 function updateCart() {
-    cartItem.innerHTML = '';
-    
-    cart.forEach(item => {
-        totalPrice += parseInt(item.price);
+    totalPrice = 0;
+    if (cart.length === 0) {
+        cartItem.innerHTML = 'Your cart is empty';
+    } else {
+        cartItem.innerHTML = '';
+        cart.forEach(item => {
+            totalPrice += item.price;
 
-        let itemHTML = `<div class="cart-item">
-            <h2>${item.name}</h2>
-            <p>Price: ${item.price}</p>
-            <i class="fa-solid fa-trash"></i>
-        </div>`;
-        
-        cartItem.innerHTML += itemHTML;
-    });
+            let itemHTML = `<div class="cart-item">
+                <h2>${item.name}</h2>
+                <p>Price: ${item.price}</p>
+                <i class="fa-solid fa-trash"></i>
+            </div>`;
 
-    let deleteIcons = cartItem.querySelectorAll('.fa-solid.fa-trash');
-    deleteIcons.forEach((deleteIcon, index) => {
-        deleteIcon.addEventListener('click', () => {
-            removeFromCart(cart[index].id);
+            cartItem.innerHTML += itemHTML;
         });
-    });
+
+        let deleteIcons = cartItem.querySelectorAll('.fa-solid.fa-trash');
+        deleteIcons.forEach((deleteIcon, index) => {
+            deleteIcon.addEventListener('click', () => {
+                removeFromCart(cart[index].id);
+            });
+        });
+    }
 
     total.textContent = totalPrice;
 }
@@ -56,6 +59,7 @@ function updateCart() {
 function removeFromCart(itemId) {
     const index = cart.findIndex((item) => item.id === itemId);
     if (index !== -1) {
+        totalPrice -= cart[index].price;
         cart.splice(index, 1);
         count--;
         document.getElementById('count').innerHTML = count;
@@ -67,9 +71,7 @@ function removeFromCart(itemId) {
 checkoutButton.addEventListener('click', () => {
     if (cart.length === 0) {
         alert('Your cart is empty. Please add chocolates to your cart before checking out.');
-     } 
-     else {
+    } else {
         alert(`Total Price: ${totalPrice}`);
     }
 });
-
